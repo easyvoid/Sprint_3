76c071f74464e754c8a671c13bed71b93a4b2213
Denis Ulanov <denzelrino@yandex.ru>
2022-04-28 11:08:26 +0300

    added improved check into test

--- src/test/java/ru/praktikum/GetOrdersWithoutCourierIdTest.java
+++ src/test/java/ru/praktikum/GetOrdersWithoutCourierIdTest.java
@@ -1,11 +1,15 @@
 package ru.praktikum;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertTrue;
 
 public class GetOrdersWithoutCourierIdTest {
 
@@ -16,11 +20,17 @@ public class GetOrdersWithoutCourierIdTest {
 
     @Test
     public void getAvailableOrdersWithoutCourierId() {
-        given().log().all()
+        Response response = given().log().all()
                 .header("Content-type", "application/json")
                 .when()
-                .get("/api/v1/orders?limit=10&page=0").then().assertThat().body("orders.id", notNullValue())
+                .get("/api/v1/orders?limit=10&page=0");
+        response.then().assertThat().body("orders.id", notNullValue())
                 .and()
                 .statusCode(200);
+
+        ArrayList<String> orders = response.then().extract().body().path("orders");
+        boolean actual = !orders.isEmpty();
+        assertTrue(actual);
+
     }
 }
